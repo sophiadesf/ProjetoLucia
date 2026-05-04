79b1d7bf1fbbb8d7481a842375f6831df375fe94
Sophiadbl <sophia.dbl@puccampinas.edu.br>
2026-05-04 09:25:40 -0300

    Atualizando código
    Finalizando o loop de repetição + exibindo as métricas finais

--- SEUC-4/Codigo/index.py
+++ SEUC-4/Codigo/index.py
@@ -1,47 +1,84 @@
 print("\n SEUC-4 \n")
-print("--------------------------------")
-numeroLeituras = int(input("Digite o número de leituras da pressão hidrodinâmica que serão realizadas no seu turno: "))
+opcao = -1 
+while not opcao == 0:
+    print("----------SELECIONE UMA OPÇÃO----------")
+    print("1 - Iniciar Leitura")
+    print("0 - Finalizar programa")
+    print("---------------------------------------\n")
 
-soma_leituras = 0
-contador = 0
-menor_pressao = None
-qtd_zona_verde = 0
+    opcao = int(input("Selecione a opção desejada: "))
+    match opcao:
+        case 1:
+            numeroLeituras = int(input("Digite o número de leituras da pressão hidrodinâmica que serão realizadas no seu turno: "))
 
-vermelha_anterior = False
-houve_travamento = False
+            soma = 0
+            contador = 0
+            menor_pressao = None
+            qtd_zona_verde = 0
 
-for i in range(numeroLeituras):
-    leitura = float(input(f"Digite a pressão da leitura {i+1}º : "))
+            vermelha_anterior = False
+            houve_travamento = False
 
-    if leitura > 150:
-        leitura_ajustada = leitura * 1.08
-    else:
-        leitura_ajustada = leitura * 1.04
+            for i in range(numeroLeituras):
+                leitura = float(input(f"Digite a pressão da leitura {i+1}º : "))
 
-    if leitura_ajustada >= 120 and leitura_ajustada <= 180:
-        qtd_zona_verde += 1
-        zona = "🟢 VERDE"
-    elif leitura < 250:
-        zona = "🟡 AMARELA"
-    else:
-        zona = "🔴 VERMELHA"
+                if leitura > 150:
+                    leitura_ajustada = leitura * 1.08
+                else:
+                    leitura_ajustada = leitura * 0.96
 
-    print("\n-------------------------------------\n")
-    print(f"Pressão ajustada: {leitura_ajustada:0.2f}\n")
-    print(f"Zona UPC:         {zona}\n")
-    print("-------------------------------------\n")
+                if leitura_ajustada >= 120 and leitura_ajustada <= 180:
+                    qtd_zona_verde += 1
+                    zona = "🟢 VERDE"
+                elif leitura < 250:
+                    zona = "🟡 AMARELA"
+                else:
+                    zona = "🔴 VERMELHA"
 
-    if zona == "🔴 VERMELHA":
-        if vermelha_anterior:
-            print("⚠️ INTERROMPER ESCOAMENTO IMEDIATO!")
-            print("\nPor questões de segurança o escoamento deve ser interrompido imediatamente. Houve duas leituras na zona vermelha.")
-            print("-------------------------------------\n")
-            houve_travamento = True
-            break
+                print("\n-------------------------------------")
+                print(f"Pressão ajustada: {leitura_ajustada:0.2f}")
+                print(f"Zona UPC:         {zona}")
+                print("---------------------------------------\n")
 
-        vermelha_anterior = True
-    else:
-        vermelha_anterior = False
-    
+                if zona == "🔴 VERMELHA":
+                    if vermelha_anterior:
+                        print("⚠️ INTERROMPER ESCOAMENTO IMEDIATO!")
+                        print("\nPor questões de segurança o escoamento deve ser interrompido imediatamente. Houve duas leituras na zona vermelha.")
+                        print("---------------------------------------\n")
+                        houve_travamento = True
+                        break
 
-        
+                    vermelha_anterior = True
+                else:
+                    vermelha_anterior = False
+                
+                soma += leitura_ajustada
+                contador += 1
+
+                if menor_pressao == None or leitura_ajustada < menor_pressao:
+                    menor_pressao = leitura_ajustada
+
+
+            media = soma/contador
+            if houve_travamento:
+                travou = "Sim"
+            else:
+                travou = "Não"
+
+            perc_zona_verde = (qtd_zona_verde/contador) * 100
+            percentual_relizado = (contador/numeroLeituras) * 100
+            print("----------- MÉTRICAS FINAIS -----------")
+            print(f"Média final: {media:0.2f}")
+            print(f"Menor pressão registrada: {menor_pressao:0.2f}")
+            print(f"Leituras na zona verde (%): {perc_zona_verde:0.2f}")
+            print(f"Houve travamento? {travou}")
+            if houve_travamento: 
+                print(f"Total leituras até o travamento (%): {percentual_relizado:0.2f}")
+            else:
+                print(f"Total leituras até a conclusão (%): {percentual_relizado:0.2f}")
+            print("---------------------------------------\n")
+        case 0:
+            print(f"Finalizando programa....")
+            print("---------------------------------------\n")
+        case _:
+            print("Digite uma opção válido!")
