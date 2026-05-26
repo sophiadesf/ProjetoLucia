0e90947b3a14b7b6dafa5f363e974389a657939f
Sophiadbl <sophia.dbl@puccampinas.edu.br>
2026-05-26 10:59:30 -0300

    Criação do projeto FluxoNorte

---
+++ FluxoNorte/index.py
@@ -0,0 +1,98 @@
+import util as util
+print("FLUXO NORTE\n")
+pedidos = {} # {"idPedido": ["Nome Cliente", "Endereço", "Prioridade", "Descricao", "status", "id_entregador"]}
+entregadores = {} # {"idEntregador": ["Nome Entregador", "Veículo", [], "Disponibilidade"]}
+opcao = -1
+while opcao != 9:
+    print("----------SELECIONE-----------")
+    print("| 1 - Cadastrar Pedido       |")
+    print("| 2 - Editar Pedido          |")
+    print("| 3 - Cadastrar Entregador   |")
+    print("| 4 - Consultar              |")
+    print("| 0 - Finalizar Sistema      |")
+    print("------------------------------")
+
+    opcao = int(input("Selecione a opção desejada: "))
+    match(opcao):
+        case 1:
+            util.limpar_tela()
+            print("FLUXO NORTE\n")
+            print("CADASTRAR PEDIDO\n")
+            id          = util.geraIdPedido()
+            nomeCliente = ""
+            endereco    = ""
+            descricao   = ""
+            prioridade  = "Pendente"
+
+            while nomeCliente == "":
+                nomeCliente = input("Digite o nome do cliente: ")
+
+            while endereco == "":
+                endereco    = input("Digite o endereço do pedido: ")
+            
+            while descricao == "":
+                descricao   = input("Digite a descrição do pedido: ")
+    
+            
+            status = util.selecioneStatus()
+
+            util.menuEntregadores(entregadores)
+            entregadorSelecionado = None
+            while entregadorSelecionado is None:
+                entregador = input("Insira o ID do entregador: ")
+                if entregador in entregadores:
+                    entregadorSelecionado = [entregador, entregadores[entregador][0]]
+                else:
+                    print("Ops! Entregador não encontrado. Insira um ID válido")
+            
+            pedidos[id] = [nomeCliente, endereco, prioridade, descricao, status, entregadorSelecionado[0]]
+            entregadores[entregadorSelecionado[0]][2].append(id)
+            util.limpar_tela()
+            print("\nPEDIDO CADASTRADO")
+            print(f" -> ID Pedido: {id}                  ")
+            print(f" -> Nome Cliente: {nomeCliente}      ")
+            print(f" -> Endereço: {endereco}             ")
+            print(f" -> Descrição: {descricao}           ")
+            print(f" -> Prioridade: {prioridade}         ")
+            print(f" -> Status: {status}                 ")
+            print(f" -> Entregador: {entregadorSelecionado[0]}  - {entregadorSelecionado[1]}")
+            print("\n\n")
+            continue
+        case 2:
+            pass
+        case 3:
+            util.limpar_tela()
+            print("FLUXO NORTE\n")
+            print("CADASTRAR PEDIDO\n")
+            id              = util.geraIdPedido()
+            nomeEntregador  = ""
+            veiculo         = ""
+            idsPedido       = []
+            disponibilidade = "Disponível"
+
+            while nomeEntregador == "":
+                nomeEntregador = input("Digite o nome do entregador: ")
+
+            veiculo = util.selecioneVeiculo()
+            
+            entregadores[id] = [nomeEntregador, veiculo, idsPedido, disponibilidade]
+            util.limpar_tela()
+            print("\nENTREGADOR CADASTRADO")
+            print(f" -> ID Entregador: {id}              ")
+            print(f" -> Nome Entregador: {nomeEntregador}")
+            print(f" -> Veículo: {veiculo}              ")
+            print(f" -> Pedidos: Este entregador ainda não possui nenhum pedido para entrega")
+            print(f" -> Disponibilidade: {disponibilidade}")
+            print("\n\n")
+            continue
+        case 4:
+            pass
+        case 0:
+            util.limpar_tela()
+            print("\nFINALIZAR SISTEMA\n")
+            if util.verificaFinalizar():
+                opcao = 9 
+            continue
+        case _:
+            util.limpar_tela()
+            print("Ops! Opção inválida, tente novamente.")
---
+++ FluxoNorte/util.py
@@ -0,0 +1,97 @@
+import os
+def limpar_tela():
+    os.system('clear')
+
+def geraIdPedido():
+    return 1
+
+def selecionePrioridade():
+    retPrioridade = ""
+    while retPrioridade == "":
+        print("-----PRIORIDADE-----")
+        print("| 0 - Normal       |")
+        print("| 1 - Alta         |")
+        print("--------------------\n")
+
+        prioridade = int(input("Digite a prioridade do pedido: "))
+        match prioridade:
+            case 0:
+                retPrioridade = "Normal"
+            case 1:
+                retPrioridade = "Alta"
+            case _:
+                print("Opção inválida! Tente novamente.")
+                
+    return retPrioridade
+
+def selecioneStatus():
+    retStatus = ""
+    while retStatus == "":
+        print("-------STATUS-------")
+        print("| 0 - Pendente      |")
+        print("| 1 - Alta          |")
+        print("--------------------\n")
+
+        prioridade = int(input("Digite a prioridade do pedido: "))
+        match prioridade:
+            case 0:
+                retStatus = "Normal"
+            case 1:
+                retStatus = "Alta"
+            case _:
+                print("Opção inválida! Tente novamente.")
+
+    return retStatus
+
+def selecioneVeiculo():
+    retVeiculo = ""
+    while retVeiculo == "":
+        print("-------VEÍCULO-------")
+        print("| 1 - Carro          |")
+        print("| 2 - Van            |")
+        print("| 3 - Moto           |")
+        print("--------------------\n")
+
+        prioridade = int(input("Selecione o tipo de veículo: "))
+        match prioridade:
+            case 1:
+                retVeiculo = "Carro"
+            case 2:
+                retVeiculo = "Van"
+            case 3:
+                retVeiculo = "Moto"
+            case _:
+                print("Opção inválida! Tente novamente.")
+
+    return retVeiculo
+
+def verificaFinalizar():
+    finalizar = False
+    resposta_valida = False
+
+    while not resposta_valida:
+        print("Você tem certeza que deseja finalizar o sistema?")
+        print("| S - Sim           |")
+        print("| N - Não           |")
+        print("--------------------\n")
+
+        opcao = input("Selecione (S/N): ").upper()
+        match opcao:
+            case "S":
+                finalizar = True
+                resposta_valida = True
+
+            case "N":
+                finalizar = False
+                resposta_valida = True
+
+            case _:
+                print("Opção inválida! Tente novamente.")
+    return finalizar
+
+
+def menuEntregadores(entregadores):
+    print("-------SELECIONE-------")
+    for ent in entregadores.keys():
+        print(f"|ID {ent} - {entregadores[ent][0]}")
+    print("-----------------------")
